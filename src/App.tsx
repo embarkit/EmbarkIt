11e303936e8588b0cad69c778dbb073f382e7d96
Tokito <tokitousan2005@gmail.com>
2023-12-30 15:41:22 +0530

    Feat: add Vite React for `gh-pages` (#12)
    * remove old codes
    
    * add vite react
    
    * config gh-pages

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Logo from "./assets/images/embarkit-logo.png";
+
+function App() {
+  return (
+    <>
+      {/* TODO: refactor this later */}
+      <main style={{"display": "grid", "placeItems": "center", "height": "100vh"}}>
+        <img width={200} src={Logo} />
+      </main>
+    </>
+  )
+}
+
+export default App
