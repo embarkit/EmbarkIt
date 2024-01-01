cfd6843557b0594bdac6954792b5c6330838e3b3
GitHub Status <154952367+Github-status@users.noreply.github.com>
2024-01-01 20:25:58 +0530

    Update vite.config.ts
    Testing

--- EmbarkIt/vite.config.ts
+++ EmbarkIt/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export defaufineConfig(
   base: "/EmbarkIt/",
   plugins: [react()],
 })
